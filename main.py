9e7879843ea345b0ab3438b4acd9c6c944c4e4fa
nicole <nicolepongchuenprapada@gmail.com>
2025-11-07 19:33:56 +0700

    ADD: save_html, save_epub

--- main.py
+++ main.py
@@ -31,12 +31,69 @@ def download_webpage(url):
         return None
 
 def extract_data(json):
+    description = json.get('description', '')
     title = json.get('title', '')
     author = json.get('user', '')
     cover = json.get('cover', '')
     tags = json.get('tags', '')
     chapters = json.get('part', '')
-    summary = json.get('description', '')
     
-    return title, author, cover, tags, chapters, summary
+    
+    return title, author, cover, tags, chapters, description
 
+def save_html(file_name, story_title, author, cover, tags, chapters, description):
+    file = open (file_name, 'w', encoding='utf-8')
+    
+    file.write(f"""
+        <html>
+        <head>
+            <meta name='title' content='{story_title}'>
+            <meta name='author' content='{author["name"]}' >
+        </head>
+        <body>
+        <div style="text-align:center;">
+            <img src="{cover}" alt="cover_image">
+        </div>
+        <br>
+        <h5 align="center">{story_title}</h5>
+        <h6 align="center">By {author["name"]} : <a href="https://www.wattpad.com/user/{author["username"]}">{author["username"]}</a></h6>
+
+        <div align="center">Tags: {tags} </div>
+        <br><br>
+        <div alight ="center">{description}<div>
+        <br><br>
+        <div align="left">
+            <h6>
+                * If chapter number or names are jumbled up, it's definitely the author's fault.
+                (Author-san, please number them correctly and in order.)<br>
+                * Converted using Wattpad2epub by Architrixs<br>
+            </h6>
+        </div>
+    """)
+    for i, chapter in enumerate(chapters):
+        print(f"getting chapter {i + 1}....")
+        chapter_url = apiV2_url + f"storytext?id={chapter['id']}"
+        chapter_content = download_webpage(chapter_url)
+        if chapter_content:
+            soup_res = bs4.BeautifulSoup(chapter_content, 'html.parser')
+            file.write(f"""
+                <br><br>
+                <h2>Chapter {i + 1}: '{chapter['title']}'</h2><br><br>
+                {soup_res.prettify()}
+                """)
+            file.write("</body></html>")
+            file.close()
+            print(f"saved {file_name}")
+
+def save_epub_file(html_file, story_title, cover):
+    print("saving EPUB...")
+    story_title = story_title.replace('/', ' ')
+    cover_image = f"{story_title}.jpg"
+    res_img = requests.get(cover, headers={'User-Agent': 'Mozilla/5.0'})
+    open(cover_image, 'wb').write(res_img.content)
+    output_file = f"{story_title}.epub"
+
+    pypandoc.convert_file(html_file, 'epub3', outputfile=output_file, extra_args=['--epub-chapter-level=2', f'--epub-cover-image={cover_image}'], sandbox=False)
+
+    os.remove(cover_image)
+    print(f"Saved {output_file}")
