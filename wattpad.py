c8690ff23a9bb6d312f0ab49fc24e0b18a60a74c
nicole <nicolepongchuenprapada@gmail.com>
2025-11-18 05:04:44 +0700

    promise will make it better

--- wattpad.py
+++ wattpad.py
@@ -1,51 +1,52 @@
-"""
-#typing with nails extension on is sooooo annoying 
-"""
-
-import os
-import requests
-import bs4 
-import pypandoc
-import re
+#! /usr/bin/python3
 import argparse
+import bs4
+import requests
 import pyperclip
+import re
+import pypandoc
+import os
 
-apiV2_url = "https://www.wattpad.com/apiv2/"
-api_V3_url = "https://www.wattpad.com/apiv3/"
-error_message = "ERROR:check the url, for valid id"
-
-url = "https://www.wattpad.com/story/379621254-inevitable-fate-18%2B-gxg"
+base_apiV2_url = "https://www.wattpad.com/apiv2/"
+base_apiV3_url = "https://www.wattpad.com/api/v3/"
+dev_error_msg = "Please check the url again, for valid story id. Contact the developer if you think this is a bug."
+url = "https://www.wattpad.com/story/374225710-calculus-of-the-heart"
 
 def get_chapter_id(url):
+    """Extracts the chapter ID from the given URL."""
     search_id = re.compile(r'\d{5,}')
     id_match = search_id.search(url)
     if id_match:
         return id_match.group()
     return None
 
+
 def download_webpage(url):
+    """Downloads the webpage content from the given URL."""
     try:
         res = requests.get(url, headers={'User-Agent': 'Mozilla/5.0'})
         res.raise_for_status()
         return res.text
-    except requests.exceptions.RequestException as esx:
-        print("There was a problem: %s % (exc)")
+    except requests.exceptions.RequestException as exc:
+        print("There was a problem: %s" % (exc))
         return None
 
-def extract_data(json):
-    description = json.get('description', '')
-    title = json.get('title', '')
-    author = json.get('user', '')
-    cover = json.get('cover', '')
-    tags = json.get('tags', '')
-    chapters = json.get('part', '')
-    
-    
-    return title, author, cover, tags, chapters, description
 
-def save_html(file_name, story_title, author, cover, tags, chapters, description):
-    file = open (file_name, 'w', encoding='utf-8')
-    
+def extract_useful_data(json_data):
+    """Extracts useful data from the JSON response."""
+    description = json_data.get('description', '')
+    tags = json_data.get('tags', '')
+    chapters = json_data.get('parts', '')
+    title = json_data.get('title', '')
+    author = json_data.get('user', '')
+    cover = json_data.get('cover', '')
+    return description, tags, chapters, title, author, cover
+
+
+def save_html_file(file_name, story_title, author, cover, tags, description, chapters):
+    """Saves the HTML file with the given data."""
+    file = open(file_name, 'w', encoding='utf-8')
+
     file.write(f"""
         <html>
         <head>
@@ -62,7 +63,8 @@ def save_html(file_name, story_title, author, cover, tags, chapters, description
 
         <div align="center">Tags: {tags} </div>
         <br><br>
-        <div alight ="center">{description}<div>
+        <div align="center">{description}</div>
+        
         <br><br>
         <div align="left">
             <h6>
@@ -73,8 +75,8 @@ def save_html(file_name, story_title, author, cover, tags, chapters, description
         </div>
     """)
     for i, chapter in enumerate(chapters):
-        print(f"getting chapter {i + 1}....")
-        chapter_url = apiV2_url + f"storytext?id={chapter['id']}"
+        print(f"Getting Chapter {i + 1}....")
+        chapter_url = base_apiV2_url + f"storytext?id={chapter['id']}"
         chapter_content = download_webpage(chapter_url)
         if chapter_content:
             soup_res = bs4.BeautifulSoup(chapter_content, 'html.parser')
@@ -82,13 +84,15 @@ def save_html(file_name, story_title, author, cover, tags, chapters, description
                 <br><br>
                 <h2>Chapter {i + 1}: '{chapter['title']}'</h2><br><br>
                 {soup_res.prettify()}
-                """)
-            file.write("</body></html>")
-            file.close()
-            print(f"saved {file_name}")
+            """)
+    file.write("</body></html>")
+    file.close()
+    print(f"Saved {file_name}")
 
-def save_epub(html_file, story_title, cover):
-    print("saving EPUB...")
+
+def save_epub_file(html_file, story_title, cover):
+    """Converts the HTML file to EPUB format and saves it."""
+    print("Generating EPUB...")
     story_title = story_title.replace('/', ' ')
     cover_image = f"{story_title}.jpg"
     res_img = requests.get(cover, headers={'User-Agent': 'Mozilla/5.0'})
@@ -99,26 +103,28 @@ def save_epub(html_file, story_title, cover):
 
     os.remove(cover_image)
     print(f"Saved {output_file}")
-    
+
+
 def main(url):
     story_id = get_chapter_id(url)
     if not story_id:
-        print(error_message)
+        print(dev_error_msg)
         return
 
-    StoryInfo = api_V3_url + f"stories/{story_id}?drafts=0&mature=1&include_deleted=1&fields=id,title,createDate,modifyDate,description,url,firstPublishedPart,cover,language,user(name,username,avatar,location,numStoriesPublished,numFollowing,numFollowers,twitter),completed,numParts,lastPublishedPart,parts(id,title,length,url,deleted,draft,createDate),tags,storyLanguage,copyright"
-    json_data  = requests.get(StoryInfo , headers={'User-Agent': 'Mozilla/5.0'}).json()
+    # Getting JSON data from Wattpad API.
+    story_info_url = base_apiV3_url + f"stories/{story_id}?drafts=0&mature=1&include_deleted=1&fields=id,title,createDate,modifyDate,description,url,firstPublishedPart,cover,language,user(name,username,avatar,location,numStoriesPublished,numFollowing,numFollowers,twitter),completed,numParts,lastPublishedPart,parts(id,title,length,url,deleted,draft,createDate),tags,storyLanguage,copyright"
+    json_data  = requests.get(story_info_url, headers={'User-Agent': 'Mozilla/5.0'}).json()
     try:
         if json_data.get('result') == 'ERROR':
             error_message = json_data.get('message', 'Unknown error')
             print(f"Error: {error_message}")
-            print(error_message)
+            print(dev_error_msg)
             return
         
         if json_data.get('error_type') :
             error_message = json_data.get('message', 'Unknown error')
             print(f"Error: {error_message}")
-            print(error_message)
+            print(dev_error_msg)
             return
         
     
@@ -130,17 +136,22 @@ def main(url):
         print(f"Error retrieving JSON data from the API: {exc}")
         return
 
-    story_title, author, cover, tags, chapters, description = extract_data(json_data)
-    
+    # Extracting useful data from JSON.
+    description, tags, chapters, story_title, author, cover = extract_useful_data(json_data)
+
+    # Saving HTML file.
     html_file_name = f"{story_title}.html"
     html_file_name = html_file_name.replace('/', ' ')
-    save_html(html_file_name, story_title, author, cover, tags, chapters, description)
-    save_epub(html_file_name, story_title, cover)
-    
-    if __name__ == "__main__":
-        parser = argparse.ArgumentParser(description='Wattpad2epub: Convert Wattpad stories to EPUB format.')
-        parser.add_argument('url', nargs='?', help='URL of the Wattpad Story')
-        args = parser.parse_args()
+    save_html_file(html_file_name, story_title, author, cover, tags, description, chapters)
+
+    # Converting HTML to EPUB.
+    save_epub_file(html_file_name, story_title, cover)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Wattpad2epub: Convert Wattpad stories to EPUB format.')
+    parser.add_argument('url', nargs='?', help='URL of the Wattpad Story')
+    args = parser.parse_args()
 
     if args.url:
         main(args.url)
